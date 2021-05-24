07bd6db155d5002aa954e8caaef28f2594ae4b88
Vinicius Hallack <viniciushallack@gmail.com>
2021-05-24 16:16:51 -0300

    Criando operacao de instanciacao

--- src/main/java/br/com/hallack/api/PrimeiraApiJavaApplication.java
+++ src/main/java/br/com/hallack/api/PrimeiraApiJavaApplication.java
@@ -1,13 +1,32 @@
 package br.com.hallack.api;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.hallack.api.domain.Categoria;
+import br.com.hallack.api.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class PrimeiraApiJavaApplication {
+public class PrimeiraApiJavaApplication implements CommandLineRunner{
+	
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(PrimeiraApiJavaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
