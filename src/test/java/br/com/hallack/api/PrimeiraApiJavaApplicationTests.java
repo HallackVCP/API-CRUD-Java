777aaadaf743dc8183ed98303c766df376f94ea1
Vinicius Hallack <viniciushallack@gmail.com>
2021-05-24 09:34:54 -0300

    Projeto criado

---
+++ src/test/java/br/com/hallack/api/PrimeiraApiJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.hallack.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiraApiJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
