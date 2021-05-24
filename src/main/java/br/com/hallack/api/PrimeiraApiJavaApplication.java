9ae81231f02392a345fb49294250ef334d8789e9
Vinicius Hallack <viniciushallack@gmail.com>
2021-05-24 16:57:02 -0300

    Produto e associacao muitos para muitos

--- src/main/java/br/com/hallack/api/PrimeiraApiJavaApplication.java
+++ src/main/java/br/com/hallack/api/PrimeiraApiJavaApplication.java
@@ -8,7 +8,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.com.hallack.api.domain.Categoria;
+import br.com.hallack.api.domain.Produto;
 import br.com.hallack.api.repositories.CategoriaRepository;
+import br.com.hallack.api.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class PrimeiraApiJavaApplication implements CommandLineRunner{
@@ -16,6 +18,9 @@ public class PrimeiraApiJavaApplication implements CommandLineRunner{
 	
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(PrimeiraApiJavaApplication.class, args);
@@ -25,8 +30,21 @@ public class PrimeiraApiJavaApplication implements CommandLineRunner{
 	public void run(String... args) throws Exception {
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 	}
 
 }
--- src/main/java/br/com/hallack/api/domain/Categoria.java
+++ src/main/java/br/com/hallack/api/domain/Categoria.java
@@ -1,11 +1,14 @@
 package br.com.hallack.api.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 
 
@@ -19,6 +22,10 @@ public class Categoria implements Serializable{
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
+	
+	
+	@ManyToMany(mappedBy = "categorias")
+	private List<Produto> produtos = new ArrayList<>();
 	
 	
 	
@@ -82,6 +89,16 @@ public class Categoria implements Serializable{
 		super();
 		this.nome = nome;
 		this.id= id;
+	}
+
+
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
 	}
 	
 	
