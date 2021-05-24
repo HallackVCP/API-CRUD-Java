0084b9d21cc2fac35323f92f8a6abb7033e68804
Vinicius Hallack <viniciushallack@gmail.com>
2021-05-24 15:18:42 -0300

    Banco de dados H2,criacao automatica db

--- src/main/java/br/com/hallack/api/domain/Categoria.java
+++ src/main/java/br/com/hallack/api/domain/Categoria.java
@@ -2,14 +2,23 @@ package br.com.hallack.api.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+
+
+@Entity
 public class Categoria implements Serializable{
-	
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
-	private String nome;
+	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	private String nome;
 	
 	
 	
