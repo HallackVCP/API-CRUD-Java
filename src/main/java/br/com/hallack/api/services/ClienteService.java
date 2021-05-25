871f891f0bdc7fc2c523a02e0c60ec2228c095ea
Vinicius Hallack <viniciushallack@gmail.com>
2021-05-25 15:43:42 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/br/com/hallack/api/services/ClienteService.java
@@ -0,0 +1,29 @@
+package br.com.hallack.api.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.hallack.api.domain.Cliente;
+import br.com.hallack.api.repositories.ClienteRepository;
+import br.com.hallack.api.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				 "Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+		
+		
+		
+	}
+
+}
