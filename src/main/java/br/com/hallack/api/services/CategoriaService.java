acf2b21c3e4538fd65cd78f4eee08a34a0f58f68
Vinicius Hallack <viniciushallack@gmail.com>
2021-05-26 16:59:00 -0300

    Listando categorias com DTO

--- src/main/java/br/com/hallack/api/services/CategoriaService.java
+++ src/main/java/br/com/hallack/api/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package br.com.hallack.api.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -49,6 +50,11 @@ public class CategoriaService {
 			
 		}
 		
+	}
+
+	public List<Categoria> findAll() {
+		
+		return repo.findAll();
 	}
 
 }
