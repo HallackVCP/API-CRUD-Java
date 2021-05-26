18cdf987a7997b0f3a5fcd0f9b4ad6be7466dc8a
Vinicius Hallack <viniciushallack@gmail.com>
2021-05-26 10:39:07 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/br/com/hallack/api/services/PedidoService.java
@@ -0,0 +1,30 @@
+package br.com.hallack.api.services;
+
+import java.util.Optional;
+import java.util.function.Supplier;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.hallack.api.domain.Pedido;
+import br.com.hallack.api.repositories.PedidoRepository;
+import br.com.hallack.api.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+	
+	
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				 "Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+		
+		
+		
+	}
+
+}
