7a6a32a8be9db06e64fd9a69943ef881dc3bb664
Vinicius Hallack <viniciushallack@gmail.com>
2021-05-26 16:02:36 -0300

    POST categoria

--- src/main/java/br/com/hallack/api/resources/CategoriaResource.java
+++ src/main/java/br/com/hallack/api/resources/CategoriaResource.java
@@ -1,14 +1,15 @@
 package br.com.hallack.api.resources;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.hallack.api.domain.Categoria;
 import br.com.hallack.api.services.CategoriaService;
@@ -27,13 +28,15 @@ public class CategoriaResource {
 		
 		Categoria obj = service.buscar(id);
 		
-		return ResponseEntity.ok().body(obj);
-		
-
-		
-		
-		
-		
+		return ResponseEntity.ok().body(obj);	
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().
+				path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 
 }
